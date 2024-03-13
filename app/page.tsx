211d5c058ced6c53b7118e9524b2769eb0363982
Marcus Neo <marcus121neo@gmail.com>
2024-03-13 22:17:28 +0800

    animation learned

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,55 @@ import styles from "./page.module.css";
 
 export default function Home() {
 	return (
-		<div className="h-screen text-white Parent bg-slate-900">
-			<div className="container px-2">
-				<div className="relative bg-red-500 rounded-lg w-32 h-32">
-					<div className="w-24 h-24 bg-green-500 rounded-lg absolute inset-2"></div>
+		<div className="m-8">
+			<button
+				className="text-white rounded-lg p-2 transition ease-in-out delay-150 
+      bg-blue-500 hover:-translate-y-1 hover:scale-110 hover:bg-indigo-500 duration-300"
+			>
+				Save Changes
+			</button>
+
+			<div className="border border-blue-300 shadow rounded-md p-4 max-w-sm w-full mx-auto">
+				<div className="animate-pulse flex space-x-4">
+					<div className="rounded-full bg-slate-700 h-10 w-10"></div>
+					<div className="flex-1 space-y-6 py-1">
+						<div className="h-2 bg-slate-700 rounded"></div>
+						<div className="space-y-3">
+							<div className="grid grid-cols-3 gap-4">
+								<div className="h-2 bg-slate-700 rounded col-span-2"></div>
+								<div className="h-2 bg-slate-700 rounded col-span-1"></div>
+							</div>
+							<div className="h-2 bg-slate-700 rounded"></div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
 	);
 
+	// return (
+	// 	<div className="h-screen text-white bg-slate-900 Parent">
+	// 		<header className="w-full border-1">Header</header>
+	// 		<div className="divide-y divide-red-500">
+	// 			<div>01</div>
+	// 			<div>02</div>
+	// 			<div>03</div>
+	// 		</div>
+	// 		<input
+	// 			type="text"
+	// 			className="border-2border-rose-600 rounded-xl h-9 w-36 outline-none"
+	// 		/>
+	// 	</div>
+	// );
+	// return (
+	// 	<div className="h-screen text-white Parent bg-slate-900">
+	// 		<div className="container px-2">
+	// 			<div className="relative bg-red-500 rounded-lg w-32 h-32">
+	// 				<div className="w-24 h-24 bg-green-500 rounded-lg absolute inset-2"></div>
+	// 			</div>
+	// 		</div>
+	// 	</div>
+	// );
 	// return (
 	// 	<div className="h-screen text-white Parent bg-slate-900">
 	// 		<div className="container px-2">
@@ -50,7 +90,6 @@ export default function Home() {
 	// 		</div>
 	// 	</div>
 	// );
-
 	// return (
 	// 	<div className="h-screen text-white Parent bg-slate-900">
 	// 		<div className="container mx-auto">
@@ -66,7 +105,6 @@ export default function Home() {
 	// 		</div>
 	// 	</div>
 	// );
-
 	// return (
 	// 	<div className="Parent">
 	// 		<div>Header</div>
@@ -85,7 +123,6 @@ export default function Home() {
 	// 		</div>
 	// 	</div>
 	// );
-
 	// return (
 	// 	<div className="Parent">
 	// 		<div>Header</div>
@@ -95,7 +132,6 @@ export default function Home() {
 	// 		</div>
 	// 	</div>
 	// );
-
 	// return (
 	// 	<div className="flex flex-col min-h-screen space-y-4">
 	// 		<div className="w-1/5 bg-red-500">Side Bar</div>
